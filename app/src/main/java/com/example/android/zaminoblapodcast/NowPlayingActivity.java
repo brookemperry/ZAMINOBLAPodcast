deeb16c6b04c1a6512cd36e51fe5dde1e4dbf6b0
brooke <brookemperry@gmail.com>
2018-07-03 23:50:14 -0400

    Navigation to home

--- app/src/main/java/com/example/android/zaminoblapodcast/NowPlayingActivity.java
+++ app/src/main/java/com/example/android/zaminoblapodcast/NowPlayingActivity.java
@@ -3,6 +3,8 @@ package com.example.android.zaminoblapodcast;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import static com.example.android.zaminoblapodcast.MainActivity.playDate;
@@ -15,7 +17,7 @@ public class NowPlayingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         String currentPodcast = "";
         String currentDate = "";
@@ -34,5 +36,16 @@ public class NowPlayingActivity extends AppCompatActivity {
         currentDateView.setText(currentDate);
 
         //TODO change play icon to pause when clicked
+
+        final ImageView status = findViewById(R.id.play);
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                status.setImageResource(R.drawable.ic_pause_black_24dp);
+
+                getActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+        });
+
     }
 }
